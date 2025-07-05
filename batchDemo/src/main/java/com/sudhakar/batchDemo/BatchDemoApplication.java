5b94fcb4273bbf7399835ea4bfdc7a9472aab5d1
sudhakar <srisudhakar56@gmail.com>
2025-07-05 13:50:15 +0530

    Welcome

---
+++ batchDemo/src/main/java/com/sudhakar/batchDemo/BatchDemoApplication.java
@@ -0,0 +1,14 @@
+package com.sudhakar.batchDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDemoApplication.class, args);
+		System.out.println("Hi sudhakar Welcome.");
+	}
+
+}
---
+++ batchDemo/src/test/java/com/sudhakar/batchDemo/BatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhakar.batchDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
